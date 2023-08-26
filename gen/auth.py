08004bbab0c45ab82f84ce7ee5c63c5fdb4d607f
Danila Zolotarev <danila-zol@list.ru>
2023-08-26 15:14:19 +0300

    Working on the API

--- gen/auth.py
+++ gen/auth.py
@@ -7,8 +7,10 @@ def requires_auth(f):
     def decorated(*args, **kwds):
         with open("gen/config.json") as configfile:
             trusted_keys = json.loads(configfile.read())['api_keys']
+
         user_auth = request.headers.get("Authorization")
         user_key = user_auth.split()[1]
+        
         if user_key in trusted_keys:
             return f(*args, **kwds)
         else:
