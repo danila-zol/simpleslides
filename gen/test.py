216294a884d6f21fc751f76dd5fe91cbefdb1005
Danila Zolotarev <danila-zol@list.ru>
2023-08-27 16:11:30 +0300

    Cleanup

--- gen/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def translate_text(text):
-    url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ru&dt=t&q={text}"
-    response = requests.get(url)
-    data = response.json()
-    return data[0][0][0] + "."
-
-print(translate_text("This is a test"))
